0132e48e2de43c026e9866d888262e60f8564d9a
unknown <aasish.shah123@gmail.com>
2019-03-06 10:28:14 +0545

    To do app modified

---
+++ src/components/TodoApp.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './TodoApp.css';
+
+class TodoApp extends Component {
+  render() {
+    return (
+      <div className="TodoApp">
+        <h1>Todo App</h1>
+      </div>
+    );
+  }
+}
+
+export default TodoApp;
